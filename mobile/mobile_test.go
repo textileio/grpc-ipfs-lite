73f48b40eb9afee8a5105060eeaae3b0637672f4
Aaron Sutula <hi@asutula.com>
2019-11-09 10:59:15 -0600

    add some tests to stop and start the server again
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- mobile/mobile_test.go
+++ mobile/mobile_test.go
@@ -23,12 +23,20 @@ func TestStop(t *testing.T) {
 	}
 }
 
-// func TestStartAgain(t *testing.T) {
-// 	port, err := Start("/tmp/mobile-ipfs-lite", false)
-// 	if err != nil {
-// 		t.Fatalf("failed to Start again: %v", err)
-// 	}
-// 	if port < 1 {
-// 		t.Fatal("invalid port again")
-// 	}
-// }
+func TestStartAgain(t *testing.T) {
+	port, err := Start("/tmp/mobile-ipfs-lite", false)
+	if err != nil {
+		t.Fatalf("failed to Start again: %v", err)
+	}
+	if port < 1 {
+		t.Fatal("invalid port again")
+	}
+}
+
+func TestStopAgain(t *testing.T) {
+	time.Sleep(10 * time.Second)
+	err := Stop()
+	if err != nil {
+		t.Fatalf("failed to StopServer again: %v", err)
+	}
+}
