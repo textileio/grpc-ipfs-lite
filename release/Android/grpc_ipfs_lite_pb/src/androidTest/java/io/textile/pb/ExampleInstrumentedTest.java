881e1cb1b38f2912816dcb66d33229c9b9044281
andrew <andrew@textile.io>
2020-01-24 14:00:32 -0800

    ci: install w/ upload step
    Signed-off-by: andrew <andrew@textile.io>

--- release/Android/grpc_ipfs_lite_pb/src/androidTest/java/io/textile/pb/ExampleInstrumentedTest.java
+++ release/Android/grpc_ipfs_lite_pb/src/androidTest/java/io/textile/pb/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
 		// Context of the app under test.
 		Context appContext = InstrumentationRegistry.getTargetContext();
 
-		assertEquals("io.textile.pb.test", appContext.getPackageName());
+		assertEquals("io.textile.grpc_ipfs_lite_pb.test", appContext.getPackageName());
 	}
 }
